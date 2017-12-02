d07d5d76d6723c8331d5233c23d9fea09f8e82fa
John Burnett <tuninator@gmail.com>
2017-12-02 21:54:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,16 @@
 require 'pry'
-def fizzbuzz
+def fizzbuzz(value)
+
+  if value % 3 == 0 && value % 5 != 0 
+    return "Fizz"
+
+  elsif value % 3 != 0  && value % 5 == 0 
+    return "Buzz"
+
+  elsif value % 3 == 0 && value % 5 == 0 
+    return "FizzBuzz"
+
+  else 
+    return nil 
   binding.pry
 end
