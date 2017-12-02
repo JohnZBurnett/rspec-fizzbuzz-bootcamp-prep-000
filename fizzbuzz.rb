b1e28329f5dbbd7caac12118d98f67c3b8f0b141
John Burnett <tuninator@gmail.com>
2017-12-02 21:55:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,19 @@ def fizzbuzz(value)
 
   if value % 3 == 0 && value % 5 != 0
     return "Fizz"
+  end 
 
   elsif value % 3 != 0  && value % 5 == 0
     return "Buzz"
+  end 
 
   elsif value % 3 == 0 && value % 5 == 0
     return "FizzBuzz"
+  end 
 
   else
     return nil
+  end 
+  
   binding.pry
 end
